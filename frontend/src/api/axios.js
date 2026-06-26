09d730158f270e5731a906bf18edc5b23d2b6aa5
nauts0698 <rudra.tiwari@timbletech.com>
2026-06-26 18:24:23 +0530

    next topic

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -14,6 +14,40 @@ api.interceptors.request.use(
     }
 )
 
+
+api.interceptors.response.use((res) => res,
+
+
+    async (error) => {
+        if (error.response.status === 401) {
+            console.log("Access Token Expired");
+
+        }
+        return Promise.reject(error)
+        // return api(error.config); yeh samjh nhi aaraha ki kahan rakhe 
+
+        // console.log(error.config);
+    })
+
+const refreshToken = localStorage.getItem("refresh");
+
+const response = await axios.post("http://127.0.0.1:8001/api/token/refresh/",
+
+    {
+
+        refresh: refreshToken
+
+    }
+
+);
+
+localStorage.setItem("access", response.data.access);
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+
 // api.interceptors.response.use(
 //     (response) => response,
 //     async (error) => {
